456c2c5037497df7412b52cf65158def25fdf831
bablu9090 <ziauddinbablu5@gmail.com>
2017-12-20 11:00:26 -0500

    to rony

--- src/my_Programs/HelloFriend.java
+++ src/my_Programs/HelloFriend.java
@@ -1,10 +1,11 @@
-package my_Programs;
+package my_programs;
 
 public class HelloFriend {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello  Friend");
+		System.out.println("Hello Friend!");
+		
 	}
 
 }
